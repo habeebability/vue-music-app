939a4e1b90be0bcf54e6a9fb72abcda504d4b0f7
Habeebability <awoyemi.habeeb@gmail.com>
2023-02-23 10:53:21 +0100

    directive with argument and modifiers

--- src/directives/icon.js
+++ src/directives/icon.js
@@ -1,6 +1,20 @@
+// directives with modifiers and arguments
 export default {
   beforeMount(el, binding) {
-    const iconClass = `fa fa-${binding.value} float-right text-green-400 text-xl`;
+    let iconClass = `fa fa-${binding.value}  text-xl`;
+
+    if (binding.arg === "full") {
+      iconClass = binding.value;
+    }
+
+    if (binding.modifiers.right) {
+      iconClass += " float-right";
+    }
+    if (binding.modifiers.yellow) {
+      iconClass += " text-yellow-400";
+    } else {
+      iconClass += " text-green-400";
+    }
 
     el.innerHTML += `<i class="${iconClass}"></i>`;
   },
