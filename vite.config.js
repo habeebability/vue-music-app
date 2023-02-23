32b958930398a8e7f7e2b1c0de6b8590e72a3f57
Habeebability <awoyemi.habeeb@gmail.com>
2023-02-23 15:46:41 +0100

    installed vue-pwa plugin, created manifest and service worker with workbox, cached files and data

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,39 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+
+import { VitePWA } from "vite-plugin-pwa";
+
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Music Player",
+        theme_color: "#ff5e3a",
+        icons: [
+          {
+            src: "assets/img/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
