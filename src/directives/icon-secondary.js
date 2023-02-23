5a9e7d78401a9a156d13edf25b029e49607d5b1c
Habeebability <awoyemi.habeeb@gmail.com>
2023-02-23 10:52:53 +0100

    added directives with object syntax

---
+++ src/directives/icon-secondary.js
@@ -0,0 +1,12 @@
+// object syntax instead of modifiers and arguments syntax
+export default {
+  beforeMount(el, binding) {
+    let iconClass = `fa fa-${binding.value.icon} text-green-400 text-xl`;
+
+    if (binding.value.right) {
+      iconClass += " float-right";
+    }
+
+    el.innerHTML += `<i class="${iconClass}"></i>`;
+  },
+};
